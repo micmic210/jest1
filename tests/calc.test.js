c4f8e995054e2cac86aa7640ccb8767209fb8299
Michiko  Inoue <inouem888@gmail.com>
2024-10-10 08:21:19 +0000

    Add arrow

--- tests/calc.test.js
+++ tests/calc.test.js
@@ -1,7 +1,7 @@
 const addition = require("../calc");
 
 describe("Calculator", () => {
-  describe("Addition function", () {
+  describe("Addition function", () => {
     test("should return 42 for 20 + 22", () => {
       expect(addition(20, 22)).tobe(42);
     });
